4cf746cf1de028575c78de5596d399389791542a
nora-winter-mayse <noramayse@gmail.com>
2022-01-26 12:45:47 -0600

    Add the session one exercise

---
+++ nora/session1/pyramids.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Provided input was not an int!")
+	}
+
+	//Iterate up the pyramid
+	for i := 1; i <= input; i++ {
+		for currentStar := 0; currentStar < i; currentStar++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")	
+	}
+
+	//Iterate down the pyramid
+	for i := input - 1; i > 0; i-- {
+		for currentStar := 0; currentStar < i; currentStar++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
---
+++ nora/session1/pyramids_improved.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Provided input was not an int!")
+	}
+
+	//Iterate up the pyramid
+	for i := 1; i <= input; i++ {
+		printPyramidRow(i)
+	}
+
+	//Iterate down the pyramid
+	for i := input - 1; i > 0; i-- {
+		printPyramidRow(i)
+	}
+}
+
+//Prints a row of stars, given the desired number of stars
+func printPyramidRow(numberOfStars int) {
+	for currentStar := 0; currentStar < numberOfStars; currentStar++ {
+		fmt.Print("*")
+	}
+	fmt.Println("")
+}
