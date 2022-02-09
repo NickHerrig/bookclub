344d0ce640e36a7076a26d786dfad1065325bb6e
brueland <wesbrueland@gmail.com>
2022-02-09 14:02:36 -0600

    fmt week2

--- week2.go
+++ week2.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
 )
 
 func main() {
 	size_of_pyramid_arg := flag.Int("size", 5, "size of pyramid")
-    flag.Parse()
+	flag.Parse()
 
-    size_of_pyramid := *size_of_pyramid_arg
-	
+	size_of_pyramid := *size_of_pyramid_arg
+
 	for i := 0; i < size_of_pyramid*2+1; i++ {
 
 		line_len := 0
 
 		if i <= size_of_pyramid {
-				line_len = i
+			line_len = i
 		}
-	    
+
 		if i > size_of_pyramid {
-				line_len = size_of_pyramid*2-i
+			line_len = size_of_pyramid*2 - i
 		}
 
 		print_string := ""
 
 		for j := 0; j <= line_len; j++ {
-            print_string += "*"
+			print_string += "*"
 		}
 
 		fmt.Print(print_string, "\n")
